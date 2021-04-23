fc9e62981d0f11c10ea9490e16b4657c6983ea65
Plotnikov_AS <plotnikovantonserg@gmail.com>
2021-04-23 16:31:14 +0300

    wip

--- src/main/java/ru/labs/second/SecondApplication.java
+++ src/main/java/ru/labs/second/SecondApplication.java
@@ -1,24 +1,12 @@
 package ru.labs.second;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.labs.second.service.ValueService;
-
-import javax.annotation.PreDestroy;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class SecondApplication {
-    private final ValueService valueService;
 
     public static void main(String[] args) {
         SpringApplication.run(SecondApplication.class, args);
     }
-
-    @PreDestroy
-    public void destroy() {
-        valueService.deleteAllValues();
-    }
-
 }
--- src/main/java/ru/labs/second/controller/MainController.java
+++ src/main/java/ru/labs/second/controller/MainController.java
@@ -1,29 +1,13 @@
 package ru.labs.second.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.labs.second.model.Value;
-import ru.labs.second.service.ValueService;
-
-import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
 public class MainController {
-    private final ValueService valueService;
 
     @GetMapping("/")
     public String mainPage() {
-        valueService.deleteAllValues();
-        return "sprizdil";
-    }
-
-    @PostMapping("/add")
-    public void add(@RequestParam(value = "newValue") Long newValue) {
-        valueService.addValue(newValue);
+        return "mainPage";
     }
 }
